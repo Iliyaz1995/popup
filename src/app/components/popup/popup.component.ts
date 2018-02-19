ee317c78fdc456ca931585302d90bddee5b180f4
iliyazUI <iliyazUI@github.com>
2018-02-20 03:18:21 +0530

    init

--- src/app/components/popup/popup.component.ts
+++ src/app/components/popup/popup.component.ts
@@ -1,7 +1,7 @@
-import { Component, OnInit, Inject,ViewEncapsulation } from '@angular/core';
-import { FormGroup, FormControl, Validators } from "@angular/forms";
+import { Component, OnInit, Inject, ViewEncapsulation } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
-import { popupService } from './services/popup.service';
+import { PopupService } from './services/popup.service';
 
 @Component({
   selector: 'app-popup',
@@ -10,38 +10,33 @@ import { popupService } from './services/popup.service';
   encapsulation: ViewEncapsulation.None
 })
 export class PopupComponent {
-  constructor(private popupService:popupService) {}
-
-    a = 1;
-    b = 3;
-    c = (this.a + this.b)
-  display: boolean = false;
-  showDialog() {
-     this.display = true;
-     };
+  constructor(private popupService: PopupService) {}
 
 
 
-  patientTaskForm:FormGroup;
-  sourceTaskOptions:Array<object> = [
-                                      {value: "",name: "-SELECT-"},
-                                      {value: "Rx",name: "Rx"},
-                                      {value: "Order",name: "Order"}
-                                     ];
+  patientTaskForm: FormGroup;
+  sourceTaskOptions: Array<object> = [
+               {value: '', name: '-SELECT-'},
+               {value: 'Rx', name: 'Rx'},
+               {value: 'Order', name: 'Order'}
+              ];
 
 
-  reasonOptions:Array<object> = [
-                                  {value: "",name: "-SELECT-"},
-                                  {value: "Insurance Diversion",name: "Insurance Diversion"},
-                                  {value: "Clinical Review Diversions",name: "Clinical Review Diversions"},
-                                  {value: "Other",name: "Counseling Diversion"}
-                                  ];
+  reasonOptions: Array<object> = [
+               {value: '', name: '-SELECT-'},
+               {value: 'Insurance Diversion', name: 'Insurance Diversion'},
+               {value: 'Clinical Review Diversions', name: 'Clinical Review Diversions'},
+               {value: 'Other', name: 'Counseling Diversion'}
+             ];
 
-  RxorOrderOptions:Array<object> = [
-                                     {value: "",name: "Please select Source"}
-                                   ];
+  RxorOrderOptions: Array<object> = [
+              {value: '', name: 'Please select Source'}
+             ];
+    
 
-
+             
+   ordersOptions: Array<object> = this.popupService.ordersOptions;
+   rxOptions: Array<object> = this.popupService.rxOptions;
 
 
   // getRxOrderOptions(){
@@ -52,54 +47,57 @@ export class PopupComponent {
   // }
 
 
-  ordersOptions:Array<object> = this.popupService.ordersOptions;
-  rxOptions:Array<object> = this.popupService.rxOptions;
 
 
 
+  submittedData: Array<object>;
+  rxOrderTitle = 'Rx/Order';
 
-  submittedData:Array<object>;
-  rxOrderTitle:string = 'Rx/Order';
 
+  display = false;
+  showDialog() {
+    this.display = true;
+  }
 
-  onPatientTaskFormSubmit(){
-      this.submittedData = this.patientTaskForm.value
+  onPatientTaskFormSubmit() {
+      this.submittedData = this.patientTaskForm.value;
       console.log(this.submittedData);
-      console.log(this.patientTaskForm)
+      console.log(this.patientTaskForm);
       this.display = true;
-  };
+  }
 
-  optionChange(){
-    var getSourceTask = this.patientTaskForm.value.sourceOfTask
-    console.log(getSourceTask)
-    if (getSourceTask === "Rx") {
+  optionChange() {
+    const getSourceTask = this.patientTaskForm.value.sourceOfTask;
+    console.log(getSourceTask);
+    if (getSourceTask === 'Rx') {
 
       this.RxorOrderOptions = this.rxOptions;
       this.rxOrderTitle = getSourceTask;
 
-    }else if(getSourceTask === "Order"){
+    } else if (getSourceTask === 'Order') {
 
       this.RxorOrderOptions = this.ordersOptions;
       this.rxOrderTitle = getSourceTask;
 
-    }else{
+    } else {
 
-      this.RxorOrderOptions = [{value: "",name: "Please select Source"}]
-      this.rxOrderTitle = "Rx/Order";
+      this.RxorOrderOptions = [{value: '', name: 'Please select Source'}];
+      this.rxOrderTitle = 'Rx/Order';
 
-    };
-  };
+    }
+  }
 
 
-  ngOnInit(){
+    // tslint:disable-next-line:use-life-cycle-interface
+    ngOnInit() {
      this.patientTaskForm = new FormGroup({
-       "sourceOfTask": new FormControl('',null),
-       "reason": new FormControl('',null),
-       "dateTime": new FormControl(null, Validators.required),
-       "orders": new FormControl('',null),
-       "priority": new FormControl(false),
-       "comments": new FormControl(`TEXT COMMENTS LINE 1,
+       'sourceOfTask': new FormControl('', null),
+       'reason': new FormControl('', null),
+       'dateTime': new FormControl(null, Validators.required),
+       'orders': new FormControl('', null),
+       'priority': new FormControl(false),
+       'comments': new FormControl(`TEXT COMMENTS LINE 1,
 TEXT COMMENTS LINE 2`, Validators.required)
     });
-  };
-};
+  }
+}
